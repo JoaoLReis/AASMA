c2db9f893bf7abea081f2312ba9b035f3be84423
ScientX <tori789@gmail.com>
2014-04-17 20:11:02 +0100

    stuff2

---
+++ Assets/Resources/Scripts/BuildingScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingScript : MonoBehaviour {
+
+	public int maxHealth = 200;
+
+	private GameObject fireEffect;
+	public GameObject prefab;
+
+	private int curHealh = 200;
+
+	void Start()
+	{
+		Object obj = Instantiate(prefab, new Vector3(transform.position.x, transform.position.y + 2.0f, transform.position.z), Quaternion.identity);
+		fireEffect = (GameObject) obj;
+		fireEffect.SetActive(false);
+	}
+
+	void Update()
+	{
+	}
+
+	public void putOutFire()
+	{
+		fireEffect.SetActive(false);
+	}
+
+	void OnMouseDown() {
+		fireEffect.SetActive(!fireEffect.activeSelf);
+	}
+	
+	public void startFire()
+	{
+		fireEffect.SetActive(true);
+	}
+
+	public bool onFire()
+	{
+		return fireEffect.activeSelf;
+	}
+
+	public bool destroyed()
+	{
+		if(curHealh == 0)
+			return true;
+		else return false;
+	}
+
+	public void turn2Wreck()
+	{
+
+	}
+}
