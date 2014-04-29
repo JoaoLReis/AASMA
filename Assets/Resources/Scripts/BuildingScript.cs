d4bdaadcd9691e9d0aad02bea7b4dfe09c269524
ScientX <tori789@gmail.com>
2014-04-29 11:34:59 +0100

    stuff

--- Assets/Resources/Scripts/BuildingScript.cs
+++ Assets/Resources/Scripts/BuildingScript.cs
@@ -3,16 +3,31 @@ using System.Collections;
 
 public class BuildingScript : MonoBehaviour {
 
+    enum Fires : int {NONE, SIMPLE, IMPROVED, GREATER};
+
 	public int maxHealth = 200;
 
 	private GameObject fireEffect = null;
-	public GameObject prefab;
+    private GameObject prefabSimple;
+    private GameObject prefabImproved;
+    private GameObject prefabGreater;
+    private int fireState;
+
+    /*********** FOR GLOBAL GAME SPEED ********/
+    private Hub hub;
+    private int gameSpeed = 1;
+    /*****************************************/
 
 	private int curHealth = 200;
 
 	void Start()
 	{
-        int randomizer = Random.Range(30, 300);
+        prefabSimple = Resources.Load("Prefab/Flame") as GameObject;
+        prefabImproved = Resources.Load("Prefab/ImprovedFlame") as GameObject;
+        prefabGreater = Resources.Load("Prefab/GreaterFlame") as GameObject;
+        hub = GameObject.FindWithTag("Hub").GetComponent<Hub>();
+        gameSpeed = hub.gameSpeed;
+        int randomizer = Random.Range(30/gameSpeed, 300/gameSpeed);
         Invoke("generateFires", randomizer);
 	}
 
@@ -29,6 +44,7 @@ public class BuildingScript : MonoBehaviour {
 	
 	void Update()
 	{
+        gameSpeed = hub.gameSpeed;
 	}
 
     public bool inNeedOfRepair()
@@ -59,17 +75,38 @@ public class BuildingScript : MonoBehaviour {
 	}
 
 	void OnMouseDown() {
-        fireEffect = (GameObject)Instantiate(prefab, new Vector3(transform.position.x, transform.position.y + 9.0f, transform.position.z), transform.rotation);
+        fireEffect = (GameObject)Instantiate(prefabSimple, new Vector3(transform.position.x, transform.position.y + 9.0f, transform.position.z), transform.rotation);
         fireEffect.transform.parent = transform;
         fireEffect.transform.Translate(3.75f * Vector3.forward);
     }
 
     private void generateFires()
     {
-        int randomizer = Random.Range(30, 150);
-        fireEffect = (GameObject)Instantiate(prefab, new Vector3(transform.position.x, transform.position.y + 9.0f, transform.position.z), transform.rotation);
+        int randomizer = Random.Range(30/gameSpeed, 300/gameSpeed);
+        
+        switch (fireState)
+        {
+            case (int)Fires.NONE:
+                Destroy(fireEffect);
+                fireEffect = (GameObject)Instantiate(prefabSimple, new Vector3(transform.position.x, transform.position.y + 9.0f, transform.position.z), transform.rotation);
+                break;
+            case (int)Fires.SIMPLE:
+                Destroy(fireEffect);
+                fireEffect = (GameObject)Instantiate(prefabImproved, new Vector3(transform.position.x, transform.position.y + 9.0f, transform.position.z), transform.rotation);
+                break;
+            case (int)Fires.IMPROVED:
+                Destroy(fireEffect);
+                fireEffect = (GameObject)Instantiate(prefabGreater, new Vector3(transform.position.x, transform.position.y + 9.0f, transform.position.z), transform.rotation);
+                break;
+            case (int)Fires.GREATER:
+                return;
+            default:
+                break;
+        }
         fireEffect.transform.parent = transform;
         fireEffect.transform.Translate(3.75f * Vector3.forward);
+
+        fireState++;
         Invoke("generateFires", randomizer);
     }
 
